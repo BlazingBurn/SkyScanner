0c77844d4b3469613acd6eac257e08a6e5071fca
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2023-01-09 00:10:31 +0100

    add file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import SearchAirport from './componants/SearchAirport/SearchAirport';
+
+import Menu from './componants/Menu';
+import { Route, Routes } from 'react-router-dom';
+import SearchFlights from './componants/SearchFlights/SearchFlights';
+import FlightDetails from './componants/FlightDetails/FlightDetails';
+
+const App: React.FC = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Menu />
+
+      <div className='backgroundHeaderAiport'>
+        <div className='text-center'>
+          <h1 className='titleHeader'>Voyage Voyage</h1>
+          <h2 className='subTitleHeader font-weight-light'>Trouver votre vol a prix réduit</h2>
+        </div>
+
+        <Routes>
+          <Route path='/' element={<SearchAirport />} />
+          <Route path="/searchFlights" element={<SearchFlights />} />
+          <Route path="/flightDetails" element={<FlightDetails />} />
+          {/* <Route path="/Favs" element={<Favs />} /> */}
+        </Routes>
+      </div>
+
+
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router  } from "react-router-dom";
+import { ThemeProvider } from 'styled-components';
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>
 );
 
